32daaa022156218ce8f22fe7980e2389e72a5110
Anna <annaefimova458@gmai.com>
2023-01-11 17:32:14 +0700

    lesson 5

--- src/pages/ChatsPage/ChatsPage.jsx
+++ src/pages/ChatsPage/ChatsPage.jsx
@@ -1,16 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useParams, Navigate } from 'react-router-dom'
 
-import { Form } from '../components/Form/Form'
-import { MessageList } from '../components/MessageList/MessageList'
-import { ChatList } from '../components/ChatList/ChatList'
+import { Form } from '../../components/Form/Form'
+import { MessageList } from '../../components/MessageList/MessageList'
+import { ChatList } from '../../components/ChatList/ChatList'
 
-import { AUTHOR } from '../constants'
+import { AUTHOR } from '../../constants'
+
+import { WithClasses } from '../../HOC/WithClasses'
+
+import styles from './ChatsPage.module.css'
 
 
 export function ChatsPage ({onAddChat, onAddMessage, messages, chats}) {
   // const [messages, setMessages] = useState([])
   const {chatId} = useParams()
+
+  const MessageListWithClass = WithClasses(MessageList)
 
   // const addMessage = (newMessage) => {
   //   console.log('newMessage', newMessage);
@@ -50,7 +56,11 @@ export function ChatsPage ({onAddChat, onAddMessage, messages, chats}) {
       <h1>Welcome to chat!</h1>
       <ChatList chats={chats} onAddChat={onAddChat} />
       <Form addMessage={handleAddMessage} />
-      <MessageList messages={chatId ? messages[chatId] : []} />
+      {/* <MessageList messages={chatId ? messages[chatId] : []} /> */}
+      <MessageListWithClass 
+        messages={chatId ? messages[chatId] : []}
+        classes={styles.border}
+      />
     </>
   )
 }
